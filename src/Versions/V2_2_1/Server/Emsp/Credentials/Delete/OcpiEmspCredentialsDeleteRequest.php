951b077d7c7381ce3a581a18a5e35ff37b35fdfd
Deniss Doroščjonoks <deniss.doroscjonoks@cubesystems.lv>
2023-02-16 11:22:32 +0200

    Duplicate previous version folder files to new version folder

---
+++ src/Versions/V2_2_1/Server/Emsp/Credentials/Delete/OcpiEmspCredentialsDeleteRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chargemap\OCPI\Versions\V2_1_1\Server\Emsp\Credentials\Delete;
+
+use Chargemap\OCPI\Common\Server\OcpiBaseRequest;
+
+class OcpiEmspCredentialsDeleteRequest extends OcpiBaseRequest
+{
+}
