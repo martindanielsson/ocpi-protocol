6e9e0589802d40615fda14ee5fcb3a3c2a7490f7
Deniss Doroščjonoks <deniss.doroscjonoks@cubesystems.lv>
2023-02-20 17:00:20 +0200

    Add new values to TokenType

--- src/Versions/V2_2_1/Common/Models/TokenType.php
+++ src/Versions/V2_2_1/Common/Models/TokenType.php
@@ -9,9 +9,13 @@ use MyCLabs\Enum\Enum;
 /**
  * @method static self RFID()
  * @method static self OTHER()
+ * @method static self AD_HOC_USER()
+ * @method static self APP_USER()
  */
 class TokenType extends Enum
 {
     public const RFID = 'RFID';
     public const OTHER = 'OTHER';
+    public const AD_HOC_USER = 'AD_HOC_USER';
+    public const APP_USER = 'APP_USER';
 }
