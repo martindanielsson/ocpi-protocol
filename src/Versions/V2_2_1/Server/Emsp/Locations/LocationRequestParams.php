e24167bef2f71f692a58090f929905eb76a8c41c
Jānis Kalniņš <janis@cubemobile.lv>
2023-03-15 17:54:20 +0200

    Throw correct location exceptions

--- src/Versions/V2_2_1/Server/Emsp/Locations/LocationRequestParams.php
+++ src/Versions/V2_2_1/Server/Emsp/Locations/LocationRequestParams.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Chargemap\OCPI\Versions\V2_2_1\Server\Emsp\Locations;
 
-use InvalidArgumentException;
+use Chargemap\OCPI\Common\Server\Errors\OcpiGenericClientError;
 
 class LocationRequestParams
 {
@@ -21,23 +21,23 @@ class LocationRequestParams
     public function __construct(string $countryCode, string $partyId, string $locationId, string $evseUid = null, string $connectorId = null)
     {
         if (empty($countryCode) || mb_strlen($countryCode) !== 2) {
-            throw new InvalidArgumentException('Country code should contain exactly 2 letters.');
+            throw new OcpiGenericClientError('Country code should contain exactly 2 letters.');
         }
 
         if (empty ($partyId) || mb_strlen($partyId) !== 3) {
-            throw new InvalidArgumentException('Party ID should contain exactly 3 characters.');
+            throw new OcpiGenericClientError('Party ID should contain exactly 3 characters.');
         }
 
         if (empty($locationId) || mb_strlen($locationId) > 36) {
-            throw new InvalidArgumentException('Location ID should contain less than 36 characters.');
+            throw new OcpiGenericClientError('Location ID should contain less than 36 characters.');
         }
 
         if ($evseUid !== null && (empty($evseUid) || mb_strlen($evseUid) > 36)) {
-            throw new InvalidArgumentException('EVSE UID should contain less than 36 characters.');
+            throw new OcpiGenericClientError('EVSE UID should contain less than 36 characters.');
         }
 
         if ($connectorId !== null && (empty($connectorId) || mb_strlen($connectorId) > 36)) {
-            throw new InvalidArgumentException('Connector ID should contain less than 36 characters.');
+            throw new OcpiGenericClientError('Connector ID should contain less than 36 characters.');
         }
 
         $this->countryCode = $countryCode;
