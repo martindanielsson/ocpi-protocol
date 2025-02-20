8722ae117a88fa2c9d921f891ba53ed13b9aced7
Martin Danielsson <martin.danielsson@eparkera.se>
2025-02-20 16:23:06 +0100

    add put existing session response

---
+++ src/Versions/V2_2_1/Server/Emsp/Sessions/Put/OcpiEmspSessionPutExistingResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chargemap\OCPI\Versions\V2_2_1\Server\Emsp\Sessions\Put;
+
+use Chargemap\OCPI\Common\Server\OcpiUpdateResponse;
+use Chargemap\OCPI\Versions\V2_2_1\Common\Models\Session;
+
+class OcpiEmspSessionPutExistingResponse extends OcpiUpdateResponse
+{
+    private Session $session;
+
+    public function __construct(Session $session, string $statusMessage = 'Session successfully updated.')
+    {
+        parent::__construct($statusMessage);
+        $this->session = $session;
+    }
+
+    protected function getData()
+    {
+        return null;
+    }
+}
