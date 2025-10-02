67337b17c74975272f50016f6cdf1cc5c5bd1ee2
Martin Danielsson <martin.danielsson@eparkera.se>
2025-10-02 16:59:27 +0200

    chore: initialize message to default empty array

--- src/Versions/V2_2/Common/Models/CommandResult.php
+++ src/Versions/V2_2/Common/Models/CommandResult.php
@@ -14,6 +14,7 @@ class CommandResult implements JsonSerializable
         CommandResultType $result
     ) {
         $this->result = $result;
+        $this->message = [];
     }
 
     public function addMessage(DisplayText $message): self
