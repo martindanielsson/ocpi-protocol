51a4f6502aafc8371012fe38b0d35443e14b05b3
Martin Danielsson <martin.danielsson@eparkera.se>
2026-02-27 18:49:37 +0100

    chore: loosen some strict schema restrictions for lazy cpos

--- src/Versions/V2_2_1/Common/Factories/GeoLocationFactory.php
+++ src/Versions/V2_2_1/Common/Factories/GeoLocationFactory.php
@@ -13,9 +13,27 @@ class GeoLocationFactory
             return null;
         }
 
+        $latitude = GeoLocationFactory::padCoordinate($json->latitude);
+        $longitude = GeoLocationFactory::padCoordinate($json->longitude);
+
         return new GeoLocation(
-            $json->latitude,
-            $json->longitude
+            $latitude,
+            $longitude
         );
+    }
+
+    /**
+     * @param string $coordinate
+     * @return string
+     *
+     * Hack for CPOs that break the contract and only send 4 or fewer decimals instead of the required 5-7.
+     */
+    public static function padCoordinate(string $coordinate): string
+    {
+        $parts = explode('.', $coordinate);
+        $decimals = $parts[1] ?? '';
+        $padded = str_pad($decimals, 5, '0');
+
+        return $parts[0] . '.' . $padded;
     }
 }
